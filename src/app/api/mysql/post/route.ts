8e6ff67c504346f8c6dccab7b35abafbf4192d22
hyeinKim <kimnieyh@gmail.com>
2024-04-11 01:00:09 +0900

    댓글 작성 및 조회 기능 구현

--- src/app/api/mysql/post/route.ts
+++ src/app/api/mysql/post/route.ts
@@ -5,9 +5,12 @@ export async function GET(request:Request) {
     try{
         const {searchParams} = new URL (request.url);
         const id = searchParams.get('id');
-        console.log(id);
+
         const results = await executeQuery(
-            "select * from post where id = ? ",[id,]);
+            "select comments.id as comment_id, comments.comment as comment," +
+            " post.title as title, post.content as content, member.name as author " +
+            "from post left join comments on post.id = comments.post_id left join member " +
+            "on member.id = comments.author_id where post.id = ? order by comments.id desc ",[id,]);
 
         console.log('post : ',results);
         return NextResponse.json(results,{status:200});
@@ -19,4 +22,25 @@ export async function GET(request:Request) {
         return  NextResponse.json(response,{status:200});
     }
 
+}
+
+export async function POST(request:Request) {
+    try {
+        const reqData = await request.json();
+        const author = reqData.author;
+        const comment = reqData.comment;
+        const postId = reqData.postId;
+
+        const result = await executeQuery(
+            "insert into comments (comment,author_id,post_id)" +
+            "values (?,?,?) ",[comment,author,postId]
+        );
+        return NextResponse.json(result,{status:200});
+    }catch (e) {
+        const response = {
+            error: (e as Error).message,
+            returnedStatus: 200
+        };
+        return NextResponse.json(response,{status:200});
+    }
 }
--- src/app/board/[postid]/page.tsx
+++ src/app/board/[postid]/page.tsx
@@ -2,30 +2,70 @@
 
 import {useRouter} from "next/navigation";
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import {ChangeEvent, FormEvent, useEffect, useState} from "react";
+import axios from "axios";
 
 function PostView({params}:{params:{postid:string}}) {
     const router = useRouter();
     const [post,setPost] = useState([{
         title: '',
         content:'',
-    }])
+        comment_id:'',
+        comment:'',
+        author:'',
+    }]);
+    const [formData, setFormData] = useState({
+        comment: '',
+        author: sessionStorage.getItem('id'),
+        postId: params.postid,
+    });
+    const [submitted, setSubmitted] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         async function fetchData(){
             try {
-                console.log(params.postid);
                 const response = await fetch(`/api/mysql/post?id=${params.postid}`);
                 const data = await response.json();
                 setPost(data);
             }catch (error) {
                 console.error('Error fetching data:',error);
+            } finally {
+                setLoading(false);
             }
         }
         fetchData();
-    },[]);
+    },[submitted]);
+
     function logout() {
         sessionStorage.setItem('id','');
         router.push('/');
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{
+        setInputValue(e.target.value);
+        const {name,value} = e.target;
+        setFormData({...formData,[name]:value});
+    }
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submit!!!");
+        try {
+            const response = await axios
+                .post('/api/mysql/post',formData)
+                .then((res)=> {
+                    console.log('res:',res);
+                    if("affectedRows" in res.data){
+                        setInputValue('');
+                        setSubmitted(true);
+                    }
+                })
+
+        }catch (e) {
+            console.error('Error:',e);
+        }
     }
 
     return (
@@ -43,25 +83,67 @@ function PostView({params}:{params:{postid:string}}) {
                 </div>
             </header>
             <div className="bg-white p-8 rounded-lg shadow-md w-full">
-                <div
-                    id="title"
-                    className="mb-2 block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6">
-                    {post[0].title}
-                </div>
+                {loading ? (
+                    <div role="status">
+                        <svg aria-hidden="true"
+                             className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                             viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"/>
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"/>
+                        </svg>
+                        <span className="sr-only">Loading...</span>
+                    </div>
+                ): (
+                    <>
+                        <div
+                            id="title"
+                            className="mb-2 block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6">
+                            {post[0].title}
+                        </div>
 
-                <div id="content"
-                     className="resize-none h-56 pl-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    {post[0].content}</div>
-                <div className="flex h-8 mb-6">
-                    <input
-                        className="pl-4 placeholder:italic border border-slate-300 w-full mt-5 h-full rounded-lg"
-                        type="text"
-                        placeholder="댓글을 입력하세요"
-                    />
-                    <button
-                        className="h-full w-1/6 text-center text-sm bg-blue-500 ml-3 rounded-lg mt-5 font-bold text-white"
-                    >작성</button>
-                </div>
+                        <div id="content"
+                             className="resize-none h-56 pl-4 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            {post[0].content}</div>
+
+
+                        <form onSubmit={handleSubmit}>
+                        <div className="flex h-8 mb-6">
+                            <input
+                                id="comment"
+                                name="comment"
+                                value={inputValue}
+                                onChange={handleChange}
+                                className="pl-4 placeholder:italic border border-slate-300 w-full mt-5 h-full rounded-lg"
+                                type="text"
+                                placeholder="댓글을 입력하세요"
+                            />
+                            <button
+                                type="submit"
+                                className="h-full w-1/6 text-center text-sm bg-blue-500 ml-3 rounded-lg mt-5 font-bold text-white"
+                            >작성</button>
+                        </div>
+                        </form>
+                        {Array.isArray(post) && post.length === 0 ? (
+                            <p>글 목록이 비어 있습니다.</p>
+                        ) : (
+                            <table className="table-auto w-full">
+                                <tbody>
+                                {post.map(({comment_id,comment,author}) => (
+                                    <tr key={comment_id} className="border-b w-full space-y-2 ">
+                                        <td>
+                                            <div className="m-4">{comment}</div>
+                                        </td>
+                                        <td className="text-center">{author}</td>
+                                    </tr>))}
+                                </tbody>
+                            </table>
+                        )}
+                    </>
+                )}
             </div>
         </main>
     );
